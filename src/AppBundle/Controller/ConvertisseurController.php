e7ec6a3e5cb2416ae2871fec9efde0da57a89d89
Arthur de Barbanson <arthurdebarbanson@gmail.com>
2017-01-15 21:04:25 +0100

    maj css recupération des valeurs ok

--- src/AppBundle/Controller/ConvertisseurController.php
+++ src/AppBundle/Controller/ConvertisseurController.php
@@ -3,24 +3,27 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ConvertisseurController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render(':default:index.html.twig');
+    }
+
+
     public function convertAction(Request $request)
     {
-
         $from=$request->get('From');
         $to=$request->get('To');
 
         $json=file_get_contents("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fd%2Fquotes.csv%3Fe%3D.csv%26f%3Dnl1d1t1%26s%3D$from$to%3DX%22%3B&format=json&callback=");
-        var_dump($json);
-        Die();
-        return $this->render('AppBundle:Convertisseur:convert.html.twig', array(
-            // ...
-        ));
+        return JsonResponse::create($json);
+
     }
 
 }
